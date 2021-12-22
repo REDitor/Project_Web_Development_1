6d0e2bbe094e65e9e2349fcbb30a174c39dc7b03
REDitor <71357600+REDitor@users.noreply.github.com>
2021-12-22 14:32:35 +0100

    stylesheet not really working

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"-->
 <!--          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">-->
+<!--    FIXME: stylesheet not working -->
     <link rel="stylesheet" href="../../../css/style.css">
     <script src="https://kit.fontawesome.com/51a8c10d85.js" crossorigin="anonymous"></script>
     <title>Curtains | Home</title>
