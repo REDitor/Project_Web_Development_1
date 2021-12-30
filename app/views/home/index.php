15614d2ddb78233d73d3d782ea2b47a4be780105
REDitor <71357600+REDitor@users.noreply.github.com>
2021-12-30 14:56:12 +0100

    ...

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,48 +2,66 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../../../css/style.css">
-    <title>Curt'ns | Home</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+	      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="../../../css/style.css">
+	<title>Curt'ns | Home</title>
 </head>
 
+<!--FIXME: fix navbar sizing-->
+<!--FIXME: -->
+
 <body class="bg-white text-dark">
-<header class="main-header">
-    <section class="navbar navbar-dark bg-dark">
-        <h1 class="logo">
-            <span class="text-danger"><i class="fas fa-video"></i> Curt</span><span class="text-light">'ns</span>
-        </h1>
-        <nav>
-            <ul>
-                <li class="nav-item float-sm-right"><a class="nav-link" href="#">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="">Movies</a></li>
-                <li class="nav-item"><a class="nav-link" href="">Shows</a></li>
-                <li class="nav-item"><a class="nav-link" href="">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="">Contact</a></li>
-            </ul>
-        </nav>
-    </section>
+	<header class="main-header">
+		<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+			<section class="container">
+				<h1 class="navbar-brand">
+					<span class="text-danger"><i class="fas fa-video"></i> Curt</span><span
+						class="text-light">'ns</span>
+				</h1>
+				<button class="navbar-toggler" data-toggle="collapse" data-target="#navBurger">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navBurger">
+					<ul class="navbar-nav ms-auto">
+						<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Movies</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Shows</a></li>
+						<li class="nav-item"><a class="nav-link" href="">About</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Contact</a></li>
+					</ul>
+				</div>
+			</section>
+		</nav>
 
-    <section class="content">
-        <h1>Too much to watch but so little time?</h1>
-        <p>No worries. With our website you can create multiple watch lists and decide what you want to watch and when you want to watch it.<br> The best news is, it's completely Free! Sign up now and start your Cinematic Journey!</p>
-        <a href="#about" class="btn"><i class="fas fa-chevron-right"></i> Create an account</a>
-    </section>
-</header>
+		<section class="content">
+			<h1>Too much to watch but so little time?</h1>
+			<p>No worries. With our website you can create multiple watch lists and decide what you want to watch and
+				when you want to watch it.<br> The best news is, it's completely Free! Sign up now and start your
+				Cinematic Journey!</p>
+			<a href="#about" class="btn"><i class="fas fa-chevron-right"></i> Create an account</a>
+		</section>
+	</header>
 
-<footer class="footer bg-dark">
-    <section class="social">
-        <a href="#"><i class="fab fa-facebook fa-2x"></i></a>
-        <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
-        <a href="#"><i class="fab fa-youtube fa-2x"></i></a>
-        <a href="#"><i class="fab fa-linkedin fa-2x"></i></a>
-    </section>
-    <p>Copyright &copy; 2021 - Curtains</p>
-</footer>
+	<footer class="footer bg-dark">
+		<section class="social">
+			<a href="#"><i class="fab fa-facebook fa-2x"></i></a>
+			<a href="#"><i class="fab fa-twitter fa-2x"></i></a>
+			<a href="#"><i class="fab fa-youtube fa-2x"></i></a>
+			<a href="#"><i class="fab fa-linkedin fa-2x"></i></a>
+		</section>
+		<p>Copyright &copy; 2021 - Curtains</p>
+	</footer>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+	        crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+	        crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+	        crossorigin="anonymous"></script>
 </body>
 </html>
