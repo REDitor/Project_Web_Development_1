dbae7cb8334ae628701bed5139430e56ee3d95cc
REDitor <71357600+REDitor@users.noreply.github.com>
2021-12-30 13:33:21 +0100

    ...

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -9,22 +9,22 @@
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../../css/style.css">
-    <title>Curtains | Home</title>
+    <title>Curt'ns | Home</title>
 </head>
 
-<body>
+<body class="bg-white text-dark">
 <header class="main-header">
     <section class="navbar navbar-dark bg-dark">
         <h1 class="logo">
-            <span><i class="fas fa-video"></i> Cur</span>tains
+            <span class="text-danger"><i class="fas fa-video"></i> Curt</span><span class="text-light">'ns</span>
         </h1>
         <nav>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="">Movies</a></li>
-                <li><a href="">Shows</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Contact</a></li>
+                <li class="nav-item float-sm-right"><a class="nav-link" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Movies</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Shows</a></li>
+                <li class="nav-item"><a class="nav-link" href="">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Contact</a></li>
             </ul>
         </nav>
     </section>
