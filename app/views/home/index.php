0fd4a60fd40bc6ca7f67d37383c1f20cbe62d15e
REDitor <71357600+REDitor@users.noreply.github.com>
2021-12-30 18:48:00 +0100

    ...

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -8,7 +8,6 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
 	      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="../../../css/style.css">
 	<title>Curt'ns | Home</title>
 </head>
 
@@ -18,32 +17,34 @@
 <body class="bg-white text-dark">
 	<header class="main-header">
 		<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+			<h1 class="ms-3">
+				<span class="text-danger"><i class="fas fa-video"></i> Curt</span><span
+					class="text-light">'ns</span>
+			</h1>
 			<section class="container">
-				<h1 class="navbar-brand">
-					<span class="text-danger"><i class="fas fa-video"></i> Curt</span><span
-						class="text-light">'ns</span>
-				</h1>
 				<button class="navbar-toggler" data-toggle="collapse" data-target="#navBurger">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navBurger">
 					<ul class="navbar-nav ms-auto">
-						<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-						<li class="nav-item"><a class="nav-link" href="">Movies</a></li>
-						<li class="nav-item"><a class="nav-link" href="">Shows</a></li>
-						<li class="nav-item"><a class="nav-link" href="">About</a></li>
-						<li class="nav-item"><a class="nav-link" href="">Contact</a></li>
+						<li class="nav-item"><a class="nav-link h6" href="#">Home</a></li>
+						<li class="nav-item"><a class="nav-link h6" href="">Movies</a></li>
+						<li class="nav-item"><a class="nav-link h6" href="">Shows</a></li>
+						<li class="nav-item"><a class="nav-link h6" href="">About</a></li>
+						<li class="nav-item"><a class="nav-link h6" href="">Contact</a></li>
 					</ul>
 				</div>
 			</section>
 		</nav>
 
-		<section class="content">
-			<h1>Too much to watch but so little time?</h1>
-			<p>No worries. With our website you can create multiple watch lists and decide what you want to watch and
-				when you want to watch it.<br> The best news is, it's completely Free! Sign up now and start your
-				Cinematic Journey!</p>
-			<a href="#about" class="btn"><i class="fas fa-chevron-right"></i> Create an account</a>
+		<section class="jumbotron jumbotron-fluid bg-light text-dark text-center pt-3 pb-3">
+			<section class="container">
+				<img src="../../../img/Cinema Seats.png" alt="Cinema Seats">
+				<h1 class="display-4">Short on time?</h1>
+				<p class="lead">Create your own watch lists and plan your movie nights.<br> The best news is, it's completely Free! Sign up now and start your
+					Cinematic Journey!</p>
+				<a href="#" class="btn btn-danger"><i class="fas fa-chevron-right"></i>  Create an account</a>
+			</section>
 		</section>
 	</header>
 
@@ -57,11 +58,14 @@
 		<p>Copyright &copy; 2021 - Curtains</p>
 	</footer>
 
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+	        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
 	        crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+	        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
 	        crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+	        integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
 	        crossorigin="anonymous"></script>
 </body>
 </html>
