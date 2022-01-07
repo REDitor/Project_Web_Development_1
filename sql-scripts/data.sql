8bafb6e192eb807f715bd0a4de0cb572e6d2102f
REDitor <71357600+REDitor@users.noreply.github.com>
2022-01-07 14:48:57 +0100

    imported database for use on laptop

--- sql-scripts/data.sql
+++ sql-scripts/data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Gegenereerd op: 11 dec 2021 om 16:50
+-- Gegenereerd op: 07 jan 2022 om 13:46
 -- Serverversie: 10.6.5-MariaDB-1:10.6.5+maria~focal
 -- PHP-versie: 7.4.25
 
@@ -27,13 +27,34 @@ SET time_zone = "+00:00";
 -- Tabelstructuur voor tabel `items`
 --
 
-CREATE TABLE if not exists `items` (
-  `itemId` int(11) NOT NULL,
-  `watchlistId` int(11) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `items` (
+  `itemId` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `writer` varchar(255) DEFAULT NULL,
-  `image` longblob DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`itemId`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `items`
+--
+
+INSERT INTO `items` (`itemId`, `title`, `writer`) VALUES
+(1, 'Tenet', 'Christopher Nolan'),
+(2, 'Dragon Ball Super: Broly', 'Tatsuya Nagamine'),
+(3, 'The Last: Naruto The Movie', 'Masashi Kishimoto'),
+(4, 'Spider-Man', 'Sam Raimi'),
+(5, 'Spider-Man 2', 'Sam Raimi'),
+(6, 'Inception', 'Christopher Nolan'),
+(7, 'Continuum', 'Simon Barry'),
+(8, 'Death Note', 'Toshiki Inoue'),
+(9, 'Naruto Shippuden', 'Masashi Kishimoto'),
+(10, 'Prison Break', 'Paul Scheuring'),
+(11, 'Squid Game', 'Hwang Dong-hyuk'),
+(12, 'The Walking Dead', 'Robert Kirkman'),
+(13, 'Avatar', 'James Cameron'),
+(14, 'The Persuit of Happyness', 'Gabrielle Muccino'),
+(15, 'Sons of Anarchy', 'Kurt Sutter'),
+(16, 'Mocro Maffia', 'Achmed Akkabi');
 
 -- --------------------------------------------------------
 
@@ -41,10 +62,25 @@ CREATE TABLE if not exists `items` (
 -- Tabelstructuur voor tabel `movies`
 --
 
-CREATE TABLE if not exists `movies` (
+CREATE TABLE IF NOT EXISTS `movies` (
   `itemId` int(11) NOT NULL,
-  `durationInMinutes` int(11) DEFAULT NULL
+  `durationInMinutes` int(11) DEFAULT NULL,
+  PRIMARY KEY (`itemId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `movies`
+--
+
+INSERT INTO `movies` (`itemId`, `durationInMinutes`) VALUES
+(1, 150),
+(2, 100),
+(3, 112),
+(4, 121),
+(5, 127),
+(6, 148),
+(13, 162),
+(14, 117);
 
 -- --------------------------------------------------------
 
@@ -52,10 +88,25 @@ CREATE TABLE if not exists `movies` (
 -- Tabelstructuur voor tabel `shows`
 --
 
-CREATE TABLE if not exists `shows` (
+CREATE TABLE IF NOT EXISTS `shows` (
   `itemId` int(11) NOT NULL,
-  `number_of_episodes` int(11) DEFAULT NULL
+  `numberOfEpisodes` int(11) DEFAULT NULL,
+  PRIMARY KEY (`itemId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `shows`
+--
+
+INSERT INTO `shows` (`itemId`, `numberOfEpisodes`) VALUES
+(7, 45),
+(8, 37),
+(9, 500),
+(10, 90),
+(11, 9),
+(12, 161),
+(15, 92),
+(16, 22);
 
 -- --------------------------------------------------------
 
@@ -63,12 +114,21 @@ CREATE TABLE if not exists `shows` (
 -- Tabelstructuur voor tabel `users`
 --
 
-CREATE TABLE if not exists `users` (
-  `userId` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`userId`, `username`, `password`, `email`) VALUES
+(3, 'TestUser1', '5d7845ac6ee7cfffafc5fe5f35cf666d', 'testuser1@test.com'),
+(4, 'TestUser2', '5d7845ac6ee7cfffafc5fe5f35cf666d', 'testuser2@test.com');
 
 -- --------------------------------------------------------
 
@@ -76,12 +136,14 @@ CREATE TABLE if not exists `users` (
 -- Tabelstructuur voor tabel `watchlists`
 --
 
-CREATE TABLE if not exists `watchlists` (
-  `watchlistId` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `watchlists` (
+  `watchlistId` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) DEFAULT NULL,
   `name` varchar(500) DEFAULT NULL,
-  `description` varchar(8000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `description` varchar(8000) DEFAULT NULL,
+  PRIMARY KEY (`watchlistId`),
+  KEY `userId` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -89,85 +151,16 @@ CREATE TABLE if not exists `watchlists` (
 -- Tabelstructuur voor tabel `watchlist_item_junction`
 --
 
-CREATE TABLE if not exists `watchlist_item_junction` (
+CREATE TABLE IF NOT EXISTS `watchlist_item_junction` (
   `watchlistId` int(11) NOT NULL,
-  `itemId` int(11) NOT NULL
+  `itemId` int(11) NOT NULL,
+  PRIMARY KEY (`watchlistId`,`itemId`),
+  KEY `itemId` (`itemId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `items`
---
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`itemId`),
-  ADD KEY `watchlistId` (`watchlistId`);
-
---
--- Indexen voor tabel `movies`
---
-ALTER TABLE `movies`
-  ADD PRIMARY KEY (`itemId`);
-
---
--- Indexen voor tabel `shows`
---
-ALTER TABLE `shows`
-  ADD PRIMARY KEY (`itemId`);
-
---
--- Indexen voor tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userId`);
-
---
--- Indexen voor tabel `watchlists`
---
-ALTER TABLE `watchlists`
-  ADD PRIMARY KEY (`watchlistId`),
-  ADD KEY `userId` (`userId`);
-
---
--- Indexen voor tabel `watchlist_item_junction`
---
-ALTER TABLE `watchlist_item_junction`
-  ADD PRIMARY KEY (`watchlistId`,`itemId`),
-  ADD KEY `itemId` (`itemId`);
-
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
-
---
--- AUTO_INCREMENT voor een tabel `items`
---
-ALTER TABLE `items`
-  MODIFY `itemId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT voor een tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT voor een tabel `watchlists`
---
-ALTER TABLE `watchlists`
-  MODIFY `watchlistId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Beperkingen voor geëxporteerde tabellen
 --
-
---
--- Beperkingen voor tabel `items`
---
-ALTER TABLE `items`
-  ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`watchlistId`) REFERENCES `watchlists` (`watchlistId`);
 
 --
 -- Beperkingen voor tabel `movies`
