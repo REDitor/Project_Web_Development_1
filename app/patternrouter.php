e9b8b79f9d1aefae23ea7e3b73e4ad4ca46f7572
REDitor <71357600+REDitor@users.noreply.github.com>
2022-01-28 20:59:09 +0100

    Heroku config added

--- app/patternrouter.php
+++ app/patternrouter.php
@@ -48,9 +48,9 @@ class PatternRouter
         $param = $explodedUri[2];
 
         //set controller directory
-        $filename = __DIR__ . '/controllers/' . $controllerName . '.php';
+        $filename = '/controllers/' . $controllerName . '.php';
         if ($api)
-            $filename = __DIR__ . '/api/controllers/' . $controllerName . '.php';
+            $filename = '/api/controllers/' . $controllerName . '.php';
 
         if (file_exists($filename)) {
             require $filename;
