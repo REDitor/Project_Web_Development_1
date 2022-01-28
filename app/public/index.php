f4682384e8060ff759fec3c7ac01921d61934949
REDitor <71357600+REDitor@users.noreply.github.com>
2022-01-28 18:01:04 +0100

    done

--- app/public/index.php
+++ app/public/index.php
@@ -10,11 +10,3 @@ $uri = trim($_SERVER['REQUEST_URI'], '/');
 
 $router = new PatternRouter();
 $router->route($uri);
-
-//region TODO
-//Must:
-//TODO: export updated database to project
-
-//Want:
-//TODO: feedback if username incorrect (login has to be implemented through javascript in that case, to be able to reuse the user.js code)
-//endregion
