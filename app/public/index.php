da4d6fea74531a8f1f969aec3f2b6a19ad5deb5c
REDitor <71357600+REDitor@users.noreply.github.com>
2022-01-28 17:21:43 +0100

    done

--- app/public/index.php
+++ app/public/index.php
@@ -1,6 +1,6 @@
 <?php
-//header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 
 use app\PatternRouter;
 
