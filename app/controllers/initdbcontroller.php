9a9599f116dd31550839aa02248515dc51760189
REDitor <71357600+REDitor@users.noreply.github.com>
2022-01-30 21:28:07 +0100

    deploy

---
+++ app/controllers/initdbcontroller.php
@@ -0,0 +1,10 @@
+<?php
+use controllers\Controller;
+
+include 'autoload.php';
+class InitDBController extends Controller
+{
+    public function index() {
+        require __DIR__ . '/../initdb.php';
+    }
+}
