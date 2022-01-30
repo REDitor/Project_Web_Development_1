defc776f47ad7f94ffc2df7d03f19de0a0d4fa1f
REDitor <71357600+REDitor@users.noreply.github.com>
2022-01-30 20:45:33 +0100

    deploy

--- app/controllers/controller.php
+++ app/controllers/controller.php
@@ -3,7 +3,7 @@ namespace controllers;
 
 class Controller {
     function displayView($model) {
-        $directory = substr(get_class($this), 0, -10);
+        $directory = strtolower(substr(get_class($this), 0, -10));
         $view = debug_backtrace()[1]['function'];
         require __DIR__ . "/../views/$directory/$view.php";
     }
--- app/public/autoload.php
+++ app/public/autoload.php
@@ -2,7 +2,7 @@
 spl_autoload_register(function($class) {
     $appDir = dirname(__DIR__) . '/';
 
-    $file = $appDir . str_replace('\\', '/', $class) . '.php';
+    $file = strtolower($appDir . str_replace('\\', '/', $class) . '.php');
 
     if (file_exists($file)) {
         require $file;
